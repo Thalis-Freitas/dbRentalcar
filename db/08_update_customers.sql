0ca07838ee60dfe9b90f2710e3baacf5a4b751bc
Thalis Freitas <ofthalis@gmail.com>
2022-12-23 20:26:56 -0300

    atualiza dados de clientes cadastrados

---
+++ db/08_update_customers.sql
@@ -0,0 +1,6 @@
+-- SQLite
+-- Edita o campo email da cliente com o nome 'Carolina'
+UPDATE CUSTOMERS SET EMAIL = 'carolina@campuscode.com.br' WHERE ID = 9;
+
+-- Edita o campo data de nascimento da cliente com o nome 'Josefa'
+UPDATE CUSTOMERS SET BIRTH_DATE = '1986-06-19' WHERE ID = 4;
