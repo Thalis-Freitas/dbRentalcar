47347c3b98daf0cf4bde3ebd6357566e9e8529c6
Thalis Freitas <ofthalis@gmail.com>
2022-12-23 20:32:05 -0300

    atualiza e remove dados registrados na tabela de carros

---
+++ db/09_update_cars.sql
@@ -0,0 +1,6 @@
+-- SQLite
+-- Edita o campo ano do automóvel com o nome 'Fiat Cronos'
+UPDATE CARS SET YEAR = 2019 WHERE NAME = 'Fiat Cronos';
+
+-- Exclui o automóvel com o nome 'Hyundai HB20 1.6'
+DELETE FROM CARS WHERE NAME = 'Hyundai HB20 1.6';
